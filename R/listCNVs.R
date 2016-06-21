bb881b5972ae6a4f1ad010c4b292cbb435f72058
Robert Scharpf <rscharpf@roberts-mbp.mtw.jhu.edu>
2016-06-21 10:55:35 -0400

    Refactored listCNVs
    Takes amplicon_graph and del_sv as arguments instead of a file path argument

--- R/listCNVs.R
+++ R/listCNVs.R
@@ -2,13 +2,14 @@
 #'
 #' @return a list
 #' @export
-#' @param dp a \code{DataPaths} object
-#' @param id length-one character vector providing sample id
-listCNVs <- function(dp, id){
-  amplicon_graphs <- readRDS(file.path(dp["2amplicons"], paste0(id, ".rds")))
-  amplicons <- ampliconRanges(amplicon_graphs)
-  deletion_sv <- readRDS(file.path(dp["1deletions"], paste0(id, ".rds")))
-  deletions <- variant(deletion_sv)
+#' @param amplicon_graph an \code{AmpliconGraph} object
+#' @param del_sv a \code{StructuralVariant} object from the deletion pipeline
+listCNVs <- function(amplicon_graph, del_sv){
+  ##amplicon_graphs <- readRDS(file)
+  ##amplicon_graphs <- readRDS(file.path(dp["2amplicons"], paste0(id, ".rds")))
+  amplicons <- ampliconRanges(amplicon_graph)
+  ##deletion_sv <- readRDS(file.path(dp["1deletions"], paste0(id, ".rds")))
+  deletions <- variant(del_sv)
   list(amplicons=amplicons,
        deletions=deletions)
 }
