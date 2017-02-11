6234a91923c2213b0bfd8951fcd70c1364ea0ca2
rscharpf <rscharpf@MacBook-Pro.local>
2017-02-11 12:33:35 -0500

    Moved test code in vignette to test_improperAlignmentParams

--- tests/testthat/test_improperAlignmentParams.R
+++ tests/testthat/test_improperAlignmentParams.R
@@ -13,3 +13,50 @@ test_that("improper alignment parameters", {
   params2 <- improperAlignmentParams(flag=flags)
   expect_identical(params, params2)
 })
+
+.test_that <- function(expr) NULL
+
+.test_that(){
+  irp <- getImproperAlignmentPairs(bview,
+                                   iparams,
+                                   mapq_thr=30,
+                                   use.mcols=TRUE)
+  prp <- getProperAlignmentPairs(bview,
+                                 pparams,
+                                 mapq_thr=30,
+                                 use.mcols=TRUE)
+  readPairs <- function(bview, mapq_thr=30){
+    irp <- getImproperAlignmentPairs(bview,
+                                     iparams,
+                                     mapq_thr=30,
+                                     use.mcols=TRUE)
+    prp <- getProperAlignmentPairs(bview,
+                                   pparams,
+                                   mapq_thr=30,
+                                   use.mcols=TRUE)
+    ##mcols(prp)$is.improper <- FALSE
+    ##mcols(irp)$is.improper <- TRUE
+  }
+
+  r1.irp <- granges(first(irp))
+  r2.irp <- granges(last(irp))
+  names(r1.irp) <- names(r2.irp) <- NULL
+  r1.irp$read <- "R1"
+  r2.irp$read <- "R2"
+  r1.irp$is.improper <- r2.irp$is.improper <- TRUE
+  r1.prp <- granges(first(prp))
+  r2.prp <- granges(last(prp))
+  names(r1.prp) <- names(r2.prp) <- NULL
+  r1.prp$read <- "R1"
+  r2.prp$read <- "R2"
+  r1.prp$is.improper <- r2.prp$is.improper <- FALSE
+  r1 <- c(r1.prp, r1.irp)
+  r2 <- c(r2.prp, r2.irp)
+  r1$pair.id <- seq_len(length(r1))
+  r2$pair.id <- seq_len(length(r2))
+  g <- c(r1, r2)
+}
+
+
+
+
