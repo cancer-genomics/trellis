04780889bab55b14b0268620b3e04dbf260521b8
rscharpf <rscharpf@MacBook-Pro.local>
2017-02-12 09:43:30 -0500

    Add pruning.mode to unit test for CNAObjects

--- tests/testthat/test_CNAObject.R
+++ tests/testthat/test_CNAObject.R
@@ -6,7 +6,7 @@ test_that("CNAObject", {
   path <- system.file("extdata", package="svbams", mustWork=TRUE)
   data(bins1kb)
   ## normalize bin counts
-  bins <- keepSeqlevels(bins1kb, "chr3")
+  bins <- keepSeqlevels(bins1kb, "chr3", pruning.mode="coarse")
   bins <- subsetByOverlaps(bins, GRanges("chr3", IRanges(59600000, 61000000)))
   bview <- BamViews(bamRanges=bins,
                     bamPaths=file.path(path, "cgov10t.bam"))
@@ -15,7 +15,7 @@ test_that("CNAObject", {
   bins$std_cnt <- binNormalize(bins)
   set.seed(123)
   gc.adj <- binGCCorrect(bins)
-  gc.adji <- as.integer(round(1000*gc_adj, 0))
+  gc.adji <- as.integer(round(1000*gc.adj, 0))
 
   preprocessdir <- tempdir()
   fname <- file.path(preprocessdir, rdsId(bview))
@@ -24,17 +24,15 @@ test_that("CNAObject", {
   paths(pview) <- fname
   setScale(pview) <- 1000
   ## a preprocess views object
-  ##devtools::load_all("~/Software/svpackages/svcnvs")
-  ##devtools::install_local("~/Software/svpackages/svcnvs")
-  dat <- svcnvs:::CNAObject(pview)
+  dat <- CNAObject(pview)
   expect_is(dat, "CNA")
 
   select <- 1:10
-  dat2 <- svcnvs:::CNAObject(pview[select, ])
+  dat2 <- CNAObject(pview[select, ])
   expect_identical(dat[select, ], dat2)
 
   bins$gc.adj <- gc.adj
   bins$id <- colnames(pview)[1]
-  dat3 <- svcnvs:::CNAObject(bins, "gc.adj")
+  dat3 <- CNAObject(bins, "gc.adj")
   expect_equal(dat$cgov10t.bam, dat3$cgov10t.bam, tolerance=0.001)
 })
