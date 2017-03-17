cd93be1bbc93f207dfa0817f160c4c8ce7b2cbfe
rscharpf <rscharpf@MacBook-Pro.local>
2017-03-17 13:05:25 -0400

    reviseEachJunction unit test missing param object
    Fixed

--- tests/testthat/test_reviseEachJunction.R
+++ tests/testthat/test_reviseEachJunction.R
@@ -3,6 +3,7 @@ context("reviseEachJunction")
 ## this is not a regression
 test_that("reviseEachJunction2", {
   sv <- readRDS("reviseEachJunction.ca5194d.rds")
+  param <- DeletionParam()
   sv2 <- hemizygousBorders(sv, param)
   expect_identical(colnames(mcols(variant(sv2))),
                    colnames(mcols(variant(sv))))
