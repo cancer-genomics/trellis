6557ea0abd12d56afa8f878c67771a5e10d3f9d5
Robert Scharpf <rscharpf@Roberts-MacBook-Pro-2.local>
2017-03-19 22:27:51 -0400

    Add unit tests for finding overlapping hem. dels
    Based on chr3 regions in CGOV10t.bam

--- tests/testthat/test_revise_seq_junctions.R
+++ tests/testthat/test_revise_seq_junctions.R
@@ -5,133 +5,127 @@ context("Revising sequence junctions")
 ## overlapping hemizygous deletions on chromosome 3
 
 .test_that("scratch", {
+  library(Rsamtools)
   library(svbams)
   bamdir <- system.file("extdata", package="svbams")
   bamfile <- file.path(bamdir, "cgov10t.bam")
   svfile <- file.path(bamdir, "cgov10t_deletions.rds")
+  expected.sv <- readRDS(svfile)
+  expected.irp <- improper(expected.sv)
+
   cbsfile <- file.path(bamdir, "cgov10t_cbs.rds")
+  lrfile <- file.path(bamdir, "cgov10t_binlevel.rds")
+  bins <- readRDS(file.path(bamdir, "cgov10t_bins1kb.rds"))
   ##
-  ## should regenerate from bamfile
+  ## todo: regenerate irp from bamfile
   ##
+
+  ## make sure all improper read pairs are available
   irpfile <- file.path(bamdir, "cgov10t_irp.rds")
-  library(svfilters.hg19)
-  data(germline_filters)
-  filters <- reduceGenomeFilters(germline_filters)
+  irp <- readRDS(irpfile)
+  mapped <- irp[!is.na(as.character(seqnames(irp)))]
+  expect_true(all(overlapsAny(expected.irp, mapped, type="within")))
+
   gr <- readRDS(cbsfile)
   gr <- gr[gr$seg.mean < log2(0.75)]
 
-  file <- file.path("structuralvar/data/alignments/0improper", id.rds)
-  aview <- AlignmentViews2(bview, file)
-  file <- file.path("structuralvar/data/preprocess/3background_adj", id.rds)
+  bview <- BamViews(bamPaths=bamfile, bamRanges=bins)
+  aview <- AlignmentViews2(bview, irpfile)
   pview <- PreprocessViews2(bview)
-  paths(pview) <- file
+  paths(pview) <- lrfile
   setScale(pview) <- 1000
+
   set.seed(123)
+  sv1 <- sv_deletions(gr=gr,
+                      aview=aview,
+                      bview=bview,
+                      pview=pview)
+  if(FALSE){
+    df <- data.frame(start=start(bins),
+                     lr=bins$log_ratios/1000)
+    library(ggplot2)
+    ggplot(df, aes(start, lr)) + geom_point(size=0.5) +
+      coord_cartesian(xlim=c(60244000, 60697000))
+    irp1 <- improper(expected.sv[1])
+    fst <- as(granges(first(irp1)), "data.frame")
+    lst <- as(granges(last(irp1)), "data.frame")
+    fst$start2 <- lst$start
+    fst$end2 <- lst$end
+    starts <- apply(fst[, c("start", "start2")], 1, min)
+    ends <- apply(fst[, c("end", "end2")], 1, max)
+    df2 <- data.frame(start=starts, end=ends, y=seq_along(starts))
+    ggplot(df, aes(start, lr)) + geom_point(size=0.5) +
+      coord_cartesian(xlim=c(60244000, 60697000))   +
+      geom_segment(data=df2, aes(x=start, xend=end, y=y, yend=y),
+                   inherit.aes=FALSE)
+  }
 
-  gr <- keepSeqlevels(gr, "chr3", pruning.mode="coarse")
-  gr <- gr[3:5]
-  names(gr) <- paste0("sv", 1:3)
-  prp <- properReadPairs(bam_path=bamPaths(bview),
-                         gr=gr, param=param)
-  prp_index <- initializeProperIndex2(gr, prp, zoom.out=1)
-  iparams <- improperAlignmentParams(which=gr, mapqFilter=0)
-  irp <- getImproperAlignmentPairs(bamPaths(bview), iparams)
-  names(irp) <- paste0("i", seq_along(irp))
-  untrace(initializeImproperIndex2, browser)
-  irp_index1 <- initializeImproperIndex2(cnv, irp, param)
-  irp_index2 <- updateImproperIndex2(cnv, irp, maxgap=4e3)
-  irp_index3 <- .match_index_variant(irp_index1, cnv, irp_index2)
-
-
-  #irp <- addImproperReadPairs2(cnv, aview, param=param)
-
-
-  irp_index1 <- initializeImproperIndex2(cnv, irp, param)
-  irp_index2 <- updateImproperIndex2(cnv, irp, maxgap=2e3)
-  irp_index3 <- .match_index_variant(irp_index1, cnv, irp_index2)
-
-  sv <- sv_deletions(gr,
-                     aview,
-                     bview,
-                     pview,
-                     filters)
-  saveRDS(sv, file=file.path("structuralvar/data/segment/1deletions", id.rds))
-
-  sv.old <- readRDS(file.path("structuralvar/data/segment/1deletions", id.rds))
-  ##sv.old <- sv.old[grep("chr3", as.character(seqnames(variant(sv.old))))]
-
-  g.old <- variant(sv.old)
-  g.old$seg.mean <- copynumber(sv.old)
-
-  g.sv <- variant(sv)
-
-  sv.old.diff <- sv.old[!overlapsAny(g.old, g.sv, type="equal")]
-  g.delta <- variant(sv.old.diff)
-  g.delta$seg.mean <- copynumber(sv.old.diff)
-  g.delta$calls <- calls(sv.old.diff)
-
-  ## chr3 region
-  g.new <- subsetByOverlaps(variant(sv), g.delta[c("sv14", "sv15")])
-  ## chrX region
-  g.new <- subsetByOverlaps(variant(sv), g.delta[c("sv1", "sv58")])
-
-
-
-  param <- DeletionParam()
-  gr_filters <- filters
-  cnv <- gr
-  cnv <- germlineFilters(cnv, gr_filters, pview, param)
-  thr <- log2(homozygousThr(param))
-  cncalls <- ifelse(cnv$seg.mean < thr, "homozygous", "hemizygous")
-  prp <- svalignments:::properReadPairs(bam_path=bamPaths(aview),
-                                        gr=cnv, param=param)
-  prp_index <- initializeProperIndex2(cnv, prp, zoom.out=1)
-  irp <- addImproperReadPairs2(cnv, aview, param=param)
-  irp_index1 <- initializeImproperIndex2(cnv, irp, param)
-  irp_index2 <- updateImproperIndex2(cnv, irp, maxgap=2e3)
-  irp_index3 <- .match_index_variant(irp_index1, cnv, irp_index2)
-
-  sv <- deletion_call(aview, pview, gr, gr_filters)
+  sv <- deletion_call(aview, pview, gr)
   calls(sv) <- rpSupportedDeletions(sv, param=param, pview=pview)
-  ##is_hemizygous <- calls(sv)=="hemizygous"
-  ##sv <- sv[!is_hemizygous]
+  sv <- removeHemizygous(sv)
   sv <- reviseEachJunction(sv, pview, aview, param)
-  sv2 <- removeSameStateOverlapping2(sv)
-  sv <- sv2
+
+  sv.revise <- revise(sv, aview, pview, param)
+  if(FALSE){
+    df3 <- data.frame(start=start(variant(sv)),
+                      end=end(variant(sv)),
+                      y=variant(sv)$seg.mean)
+    ggplot(df, aes(start, lr)) + geom_point(size=0.5) +
+      coord_cartesian(xlim=c(60244000, 60697000))   +
+      geom_segment(data=df2, aes(x=start, xend=end, y=y, yend=y),
+                   inherit.aes=FALSE) +
+      geom_segment(data=df3, aes(x=start, xend=end, y=y, yend=y),
+                   inherit.aes=FALSE)
+  }
   copynumber(sv) <- granges_copynumber(variant(sv), pview)
   calls(sv) <- rpSupportedDeletions(sv, param=param, pview=pview)
-  if(FALSE){
-    is_hemizygous <- calls(sv) == "hemizygous"
-    sv <- sv[!is_hemizygous]
-    if(length(sv) == 0) return(sv)xo
-  }
-  sv2 <- removeSameStateOverlapping2(sv)
-
-  sv <- sv2
   indexImproper(sv) <- updateImproperIndex(sv, maxgap=500)
   calls(sv) <- rpSupportedDeletions(sv, param, pview=pview)
-  is_hemizygous <- calls(sv) == "hemizygous"
-  ##sv <- sv[!is_hemizygous]
   sv2 <- leftHemizygousHomolog(sv, pview, param)
   sv3 <- rightHemizygousHomolog(sv2, pview, param)
   calls(sv3) <- rpSupportedDeletions(sv3, param, pview)
-  sv4 <- sv3[calls(sv3) != "hemizygous"]
-
-  sv5 <- refineHomozygousBoundaryByHemizygousPlus(sv4)
+  message("Refining homozygous boundaries by spanning hemizygous+")
+  sv5 <- refineHomozygousBoundaryByHemizygousPlus(sv3)
   sv6 <- callOverlappingHemizygous(sv5)
-  sv7 <- removeSameStateOverlapping2(sv6) 
+  sv7 <- removeSameStateOverlapping2(sv6)
+  sv <- removeHemizygous(sv7)
+  expect_identical(sv, sv.revise)
 
-  sv8 <- SVFilters(sv7, gr_filters, pview, param=param)
-  sv9 <- groupSVs(sv8)
-  id <- names(aview)
-  sv9 <- allProperReadPairs(sv9, param, bfile=bamPaths(bview), zoom.out=1)
+  i <- order(variant(sv))
+  j <- order(variant(expected.sv))
+  g <- granges(variant(sv[i]))
+  g.expected <- granges(variant(expected.sv[j]))
+  expect_identical(start(g), start(g.expected))
+  expect_identical(end(g), end(g.expected))
+  expect_identical(calls(sv)[i], calls(expected.sv)[j])
 
-  chr3 <- keepSeqlevels(variant(sv), "chr3", pruning.mode="coarse")
+  sv.finalize <- finalize_deletions(sv, gr_filters,
+                                    pview, bview,
+                                    param)
+  expect_identical(variant(sv.finalize),
+                   variant(sv))
 
-  cncalls <- gsub("\\+", "", calls(sv))
-  is.homdel <- cncalls == "homozygous"
-  sv.homdel <- sv[is.homdel]
-  sv.hemdel <- sv[!is.homdel]
-  sv.hemdel <- adjudicateHemizygousOverlap2(sv.hemdel)
-  sv.homdel <- adjudicateHomozygousOverlap2(sv.homdel)
+  expect_identical(variant(sv.finalize),
+                   variant(sv1))
+  if(FALSE){
+    df3 <- data.frame(start=start(variant(sv)),
+                      end=end(variant(sv)),
+                      y=variant(sv)$seg.mean)
+    ggplot(df, aes(start, lr)) + geom_point(size=0.5) +
+      coord_cartesian(xlim=c(60244000, 60697000))   +
+      geom_segment(data=df2, aes(x=start, xend=end, y=y, yend=y),
+                   inherit.aes=FALSE) +
+      geom_segment(data=df3, aes(x=start, xend=end, y=y, yend=y),
+                   inherit.aes=FALSE)
+
+    df4 <- data.frame(start=start(variant(expected.sv)),
+                      end=end(variant(expected.sv)),
+                      y=copynumber(expected.sv))
+    ggplot(df, aes(start, lr)) + geom_point(size=0.5) +
+      coord_cartesian(xlim=c(60244000, 60697000))   +
+      geom_segment(data=df2, aes(x=start, xend=end, y=y, yend=y),
+                   inherit.aes=FALSE) +
+      geom_segment(data=df4, aes(x=start, xend=end, y=y, yend=y),
+                   inherit.aes=FALSE, color="blue")
+  }
 })
