9b5382eab5f7da7fb28caab61e65207ca950cff5
rscharpf <rscharpf@MacBook-Pro.local>
2017-03-20 11:42:49 -0400

    Add scratch work in revise_seq_junctions unit tests

--- tests/testthat/test_revise_seq_junctions.R
+++ tests/testthat/test_revise_seq_junctions.R
@@ -127,3 +127,83 @@ test_that("overlappingHemizgyous", {
                    inherit.aes=FALSE, color="blue")
   }
 })
+
+.test_that <- function(name, expr) NULL
+.test_that("scratch", {
+  g.old <- variant(sv.old)
+  g.cur <- variant(sv)
+  sv.delta <- sv[!overlapsAny(g.cur, g.old)]
+
+
+  library(ggplot2)
+  suppressPackageStartupMessages(library(gridExtra))
+  library(scales)
+
+  segs <- readRDS(file.path("structuralvar/data/segment/0cbs", id.rds))
+  segs <- segs[segs$seg.mean < log2(0.75)]
+  suppressPackageStartupMessages(library(SummarizedExperiment))
+  ##
+  ## region of interest (roi)
+  ##
+  roi <- variant(sv.delta[1])
+  roi <- keepSeqlevels(roi, as.character(seqnames(roi)))
+  expand <- 200e3
+  roi2 <- GRanges(seqnames(roi), IRanges(start(roi)-expand,
+                                         end(roi) + expand))
+  seqinfo(roi2) <- seqinfo(roi)
+  pview2 <- pview[queryHits(findOverlaps(rowRanges(pview), roi2)), ]
+  segs <- keepSeqlevels(segs, seqlevels(roi), pruning.mode="coarse")
+  segs.df <- as(segs, "data.frame")
+  df <- data.frame(logr=assays(pview2)[, 1],
+                   start=start(rowRanges(pview2)))
+ylim <- c(-9, 2)
+df$logr <- svpreprocess::threshold(df$logr, ylim)
+brks <- pretty(df$start, n=8)
+region <- subsetByOverlaps(segs, roi)
+region <- region[region$seg.mean < -1]
+region <- as.data.frame(region)
+xlim <- c(start(roi2), end(roi2))
+
+A <- ggplot(df, aes(start, logr)) +
+  geom_point(size=1, color="gray50") +
+  scale_x_continuous(expand=c(0,0), breaks=brks, labels=brks/1e6)+
+  scale_y_continuous(expand=c(0,0)) +
+  geom_segment(data=segs.df,
+               aes(x=start, xend=end, y=seg.mean, yend=seg.mean),
+               size=1) +
+  coord_cartesian(xlim=xlim, ylim=ylim) +
+  ylab(expression(log[2]~ratio)) +
+  geom_rect(data=region,
+            aes(xmin=start, xmax=end, ymin=-Inf, ymax=+Inf),
+            fill="steelblue", color="transparent", alpha=0.3,
+            inherit.aes=FALSE) +
+  theme(axis.text=element_text(size=10),
+        axis.text.x=element_blank()) + xlab("") +
+  annotate("text", x=xlim[1] + 15e3, y=-8, label="chr15", size=3)
+A1 <- ggplotGrob(A)
+
+  rps <- thinReadPairs(sv.delta[1])
+  library(svfilters.hg19)
+  ##trace(svcnvs:::meltReadPairs, browser)
+  rps <- svcnvs:::meltReadPairs(rps)
+colors <- c("#0072B2", "#009E73")
+p <- ggplot(rps, aes(ymin=readpair-0.2, ymax=readpair+0.2,
+                xmin=start/1e6, xmax=end/1e6, color=read,
+                fill=read, group=readpair)) +
+  geom_rect() +
+  xlim(c(min(rps$start), max(rps$end))/1e6) +
+  geom_line(aes(x=start/1e6, y=readpair)) +
+  ylab("read pair index") +
+  scale_x_continuous(breaks=pretty_breaks(5)) +
+  geom_rect(data=region,
+            aes(xmin=start/1e6, xmax=end/1e6, ymin=-Inf, ymax=+Inf),
+            fill="steelblue", color="transparent", alpha=0.2,
+            inherit.aes=FALSE) +
+  scale_color_manual(values=colors) +
+  scale_fill_manual(values=colors) + 
+  xlab("Mb") +
+  theme(axis.text.x=element_text(size=7)) +
+  guides(fill=FALSE, color=FALSE) +
+  geom_vline(xintercept=c(start(roi)/1e6, end(roi)/1e6), linetype="dashed")
+  B <- ggplotGrob(p)
+})
