87e0d838b34b1db2ac1724cf67c31b2101a8b42c
Robert Scharpf <rscharpf@Roberts-MacBook-Pro-2.local>
2017-03-22 08:31:10 -0400

    Add slot for tumor_purity in DeletionParam
    Slots for hemizygous and homozygous cutoffs should be specified on log2 scale

--- R/DeletionParam-class.R
+++ R/DeletionParam-class.R
@@ -52,7 +52,9 @@ NULL
 #' DeletionParam()
 #' @rdname DeletionParam-class
 #' @export
-setClass("DeletionParam", representation(tag_density_homozygous_thr="numeric",
+setClass("DeletionParam", representation(homozygous_thr="numeric",
+                                         hemizygous_thr="numeric",
+                                         tumor_purity="numeric",
                                          min_width="numeric",
                                          max_width="numeric",
                                          min_gapwidth="numeric",
@@ -65,44 +67,43 @@ setClass("DeletionParam", representation(tag_density_homozygous_thr="numeric",
                                          bam_seqlevels_style="character"))
 
 
-#' @param max_tag_density length-one numeric vector indicating the
-#' maximum fold change for calling deletions (assumes threshold
-#' provided using linear-scale).
-#' @param tag_density_homozygous_thr length-one numeric vector
-#' indicating the maximum fold change expected for a homozygous
-#' deletion
-#' @param min_width length-one numeric vector indicating the minimum
-#' width of a CNV segment.
-#' @param max_width length-one numeric vector indicating the maximum
-#' width of a CNV segment.
-#' @param min_gapwidth length-one numeric vector used to reduce
-#' deletion calls. Provided that the segment means of adjacent
-#' deletions are qualitatively similar, two deletions less than
-#' min_gapwidth apart will be merged into a single CNV.
-#' @param min_RPs_in_hemizygous length-one numeric vector indicating
-#' the minimum number of rearranged read pairs that can be spanned by
-#' a hemizygous deletion. [PARAMETER NOT USED]
-#' @param max_RPs_in_homozygous length-one numeric vector indicating
-#' the maximum number of read pairs allowed to be spanned by a
-#' homozygous deletion.  [PARAMETER NOT USED]
-#' @param nflanking_hemizygous length-one numeric vector indicating the
-#' minimum number of rearranged RPs required for calling a hemizygous
-#' deletion with higher confidence.  Hemizygous deletions with
-#' rearranged RPs are given higher priority over hemizygous deletions
-#' supported only by the tag density.
-#' @param nflanking_homozygous length-one numeric vector indicating the
-#' minimum number of rearranged RPs required for calling a homozygous
-#' deletion with higher confidence.  Homozygous deletions with
-#' rearranged RPs are given higher priority over homozygous deletions
-#' supported only by the tag density.
+#' @param homozygous_thr length-one numeric vector indicating the maximum log2
+#'   ratio expected for a homozygous deletion
+#' @param hemizygous_thr length-one numeric vector indicating the maximum log2
+#'   ratio expected for a hemizygous deletion
+#' @param tumor_purity length-one numeric vector in [0, 1]
+#' @param min_width length-one numeric vector indicating the minimum width of a
+#'   CNV segment.
+#' @param max_width length-one numeric vector indicating the maximum width of a
+#'   CNV segment.
+#' @param min_gapwidth length-one numeric vector used to reduce deletion calls.
+#'   Provided that the segment means of adjacent deletions are qualitatively
+#'   similar, two deletions less than min_gapwidth apart will be merged into a
+#'   single CNV.
+#' @param min_RPs_in_hemizygous length-one numeric vector indicating the minimum
+#'   number of rearranged read pairs that can be spanned by a hemizygous
+#'   deletion. [PARAMETER NOT USED]
+#' @param max_RPs_in_homozygous length-one numeric vector indicating the maximum
+#'   number of read pairs allowed to be spanned by a homozygous deletion.
+#'   [PARAMETER NOT USED]
+#' @param nflanking_hemizygous length-one numeric vector indicating the minimum
+#'   number of rearranged RPs required for calling a hemizygous deletion with
+#'   higher confidence. Hemizygous deletions with rearranged RPs are given
+#'   higher priority over hemizygous deletions supported only by the tag
+#'   density.
+#' @param nflanking_homozygous length-one numeric vector indicating the minimum
+#'   number of rearranged RPs required for calling a homozygous deletion with
+#'   higher confidence. Homozygous deletions with rearranged RPs are given
+#'   higher priority over homozygous deletions supported only by the tag
+#'   density.
 #' @param maxRatioObservedToExpected length-one numeric vector [IGNORED?]
-#' @param max_proportion_in_filter length-one numeric vector indicating
-#' the maximum proportion of a CNV that can be covered by one of the
-#' filters.  The filters are germline CNV, low mappability, low GC /
-#' unassembled regions, and outliers observed in a bin for 2 or more
-#' normal samples.
+#' @param max_proportion_in_filter length-one numeric vector indicating the
+#'   maximum proportion of a CNV that can be covered by one of the filters. The
+#'   filters are germline CNV, low mappability, low GC / unassembled regions,
+#'   and outliers observed in a bin for 2 or more normal samples.
 #' @param bam_seqlevels_style character string indicating style of seqnames
-#' @param build length-1 character vector indicating genome build (must be hg18 or hg19)
+#' @param build length-1 character vector indicating genome build (must be hg18
+#'   or hg19)
 #' @export
 #' @return a \code{DeletionParam} object
 #' @rdname DeletionParam-class
@@ -163,10 +164,6 @@ maximumWidth <- function(object) object@max_width
 #' @export
 maximumProportionInFilter <- function(object) object@max_proportion_in_filter
 
-#' @keywords internal
-#' @rdname DeletionParam-class
-#' @export
-maxTagDensity <- function(object) object@max_tag_density
 
 #' @keywords internal
 #' @rdname DeletionParam-class
