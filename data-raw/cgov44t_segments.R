42674d67c08797908d9c37f7cca053a2f8d90d59
rscharpf <rscharpf@MacBook-Pro.local>
2017-03-23 19:11:41 -0400

    Change code and extdata object for cgov44t_segments

--- data-raw/cgov44t_segments.R
+++ data-raw/cgov44t_segments.R
@@ -1,6 +1,8 @@
 gr <- readRDS("~/Dropbox/OvarianCellLines/structuralvar/data/segment/0cbs/CGOV44T.bam.rds")
-gr <- keepSeqlevels(gr, c("chr5", "chr8"), pruning.mode="coarse")
-saveRDS(gr, file="../inst/extdata/cgov44t_segments.rds")
+segs <- keepSeqlevels(gr, c("chr5", "chr8", "chr15"), pruning.mode="coarse")
+##gr <- readRDS("~/Dropbox/OvarianCellLines/structuralvar/data/segment/0cbs/CGOV44T.bam.rds")
+##gr <- keepSeqlevels(gr, c("chr5", "chr8"), pruning.mode="coarse")
+saveRDS(segs, file="../inst/extdata/cgov44t_segments.rds")
 
 ## for unit test
 library(Rsamtools)
