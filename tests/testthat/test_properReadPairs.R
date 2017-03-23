3708201e4ba33e12e42d5b80498949812fb093f7
Robert Scharpf <rscharpf@Roberts-MacBook-Pro-2.local>
2017-03-22 20:41:09 -0400

    Add unit tests for properReadPairs

--- tests/testthat/test_properReadPairs.R
+++ tests/testthat/test_properReadPairs.R
@@ -2,8 +2,18 @@ context("properReadPairs")
 
 test_that("properReadPairs",{
   library(svbams)
-  svfile <- file.path(bamdir, "cgov10t_deletions.rds")
-  bamdir <- system.file("extdata", package="svbams")
+  bamdir <- system.file("extdata", package="svbams", mustWork=TRUE)
+  bam.file <- file.path(bamdir, "cgov10t.bam")
+  ##svfile <- file.path(bamdir, "cgov10t_deletions.rds")
   bins <- readRDS(file.path(bamdir, "cgov10t_bins1kb.rds"))
-  bview <- BamViews(bamPaths=bamfile, bamRanges=bins)
+  proper <- properReadPairs(bam.file, bins)
+
+  ## does not do anything if all bins are less than 20kb
+  bins2 <- thinReadPairQuery(bins)
+  expect_equivalent(bins2, bins)
+
+  segments <- variant(readRDS(file.path(bamdir, "cgov10t_deletions.rds")))
+  ## for larger segments, focus on the border 
+  segs <- thinReadPairQuery(segments)
+  expect_identical(length(segs), 10L)
 })
