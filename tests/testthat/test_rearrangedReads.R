4e5703b1b4c509af03b451a55ce81ab1e6c670b2
rscharpf <rscharpf@MacBook-Pro.local>
2017-04-07 08:04:00 -0400

    Delete the AlignmentViews.R and blat.R from data-raw/
    Also removes the unit test that relied on this data

--- tests/testthat/test_rearrangedReads.R
+++ tests/testthat/test_rearrangedReads.R
@@ -1,18 +1,18 @@
-test_that("overlapsBoth", {
-  data(rearrangement_list)
-  data(blat_unmapped)
-  data(rearrangement_list)
-  rearranged_reads <- rearrangedReads(rearrangement_list, blat_unmapped, 500)
-  expect_is(rearranged_reads, "GRangesList")
-  ## only one rearrangement supported
-  expect_identical(length(rearranged_reads), 1L)
-  expect_true(length(rearrangement_list[names(rearranged_reads)])==1)
-  ## number split reads
-  n_splitreads <- length(split(rearranged_reads[[1]], rearranged_reads[[1]]$qname))
-  expect_identical(n_splitreads, 32L)
-
-  ## this should exit gracefully with no rearranged reads identified
-  rearranged_reads <- rearrangedReads(rearrangement_list, blat_unmapped[1:10, ], 500)
-  expect_identical(length(rearranged_reads), 0L)
-  expect_is(rearranged_reads, "GRangesList")
-})
+##test_that("overlapsBoth", {
+##  data(rearrangement_list)
+##  data(blat_unmapped)
+##  data(rearrangement_list)
+##  rearranged_reads <- rearrangedReads(rearrangement_list, blat_unmapped, 500)
+##  expect_is(rearranged_reads, "GRangesList")
+##  ## only one rearrangement supported
+##  expect_identical(length(rearranged_reads), 1L)
+##  expect_true(length(rearrangement_list[names(rearranged_reads)])==1)
+##  ## number split reads
+##  n_splitreads <- length(split(rearranged_reads[[1]], rearranged_reads[[1]]$qname))
+##  expect_identical(n_splitreads, 32L)
+##
+##  ## this should exit gracefully with no rearranged reads identified
+##  rearranged_reads <- rearrangedReads(rearrangement_list, blat_unmapped[1:10, ], 500)
+##  expect_identical(length(rearranged_reads), 0L)
+##  expect_is(rearranged_reads, "GRangesList")
+##})
