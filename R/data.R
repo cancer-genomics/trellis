e3a3334bce8e0c0e7f110aad93303172ad6bed29
Daniel Bruhm <danielbruhm@gmail.com>
2017-06-29 15:41:41 -0400

    Add dummy docs for rda files in /data

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' An AmpliconGraph object
+#'
+#' @name amplicon_graph
+#' @docType data
+NULL
+
+#' A StructuralVariant object
+#'
+#' @name deletion
+#' @docType data
+NULL
+
+#' A StructuralVariant object
+#'
+#' @name deletions2
+#' @docType data
+NULL
+
+#' A GRanges object of segmentation data
+#'
+#' @name segments
+#' @docType data
+NULL
