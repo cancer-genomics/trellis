b18f2042b669e52ffc083b77251487d12e0f780a
Daniel Bruhm <danielbruhm@gmail.com>
2017-08-04 11:01:12 -0400

    Write unit test for replacing the improper slot in Rearrangement objects

---
+++ tests/testthat/test_Rearrangement.R
@@ -0,0 +1,11 @@
+context("Rearrangement")
+
+test_that("improper<-", {
+  data(rear_list)
+  r <- rear_list[[1]]
+  orig <- improper(r)
+  expect_equivalent(length(orig), 26) # Make sure the rear_list object doesn't change over time 
+  sub <- orig[1:2]
+  improper(r) <- sub
+  expect_equivalent(improper(r), sub)
+})
