9787ebf8be3986b93bb8a991fb10578bdec699de
Robert Scharpf <rscharpf@Roberts-MacBook-Pro-2.local>
2018-03-21 07:22:40 -0400

    Import seqlengths<-

--- R/help.R
+++ R/help.R
@@ -14,7 +14,7 @@
 #' @importFrom S4Vectors SimpleList mcols mcols<- queryHits subjectHits DataFrame
 #' @importFrom S4Vectors queryHits subjectHits isTRUEorFALSE
 #' @importMethodsFrom S4Vectors elementType elementNROWS
-#' @importMethodsFrom GenomeInfoDb seqnames seqlevels seqlevels<- seqinfo seqinfo<- seqlengths
+#' @importMethodsFrom GenomeInfoDb seqnames seqlevels seqlevels<- seqinfo seqinfo<- seqlengths seqlengths<-
 #' @importMethodsFrom GenomeInfoDb genome genome<- seqlevelsStyle<- seqlevelsStyle
 #' @importFrom GenomeInfoDb keepSeqlevels seqlevels seqinfo<- seqinfo
 #' @importFrom IRanges IRanges LogicalList
