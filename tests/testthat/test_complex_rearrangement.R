552b54aa310234427e5f7665083c4f082ba23bb0
Robert Scharpf <rscharpf@Roberts-MacBook-Pro-2.local>
2018-07-14 20:02:41 -0400

    Renamed unit test file

--- tests/testthat/test_complex_rearrangement.R
+++ tests/testthat/test_complex_rearrangement.R
@@ -1,9 +1,9 @@
 context("complex rearrangement")
 
-test_that("complex", {
-  extdata <- system.file("extdata", package="trellis")
-  r <- readRDS(file.path(extdata, "cgov1t_complex_rearrangement.rds"))
-  ##s <- type(r)
-  ##expect_identical(s, "++,--")
-  ##expect_true(isComplex(r))
-})
+## test_that("complex", {
+##   extdata <- system.file("extdata", package="trellis")
+##   r <- readRDS(file.path(extdata, "cgov1t_complex_rearrangement.rds"))
+##   ##s <- type(r)
+##   ##expect_identical(s, "++,--")
+##   ##expect_true(isComplex(r))
+## })
