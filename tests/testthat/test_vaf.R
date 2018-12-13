8693119c98d7be8937953e89a5d6943fbce39e2c
rscharpf <rscharpf@MacBook-Pro.local>
2018-12-13 10:38:20 -0500

    update unit test

--- tests/testthat/test_vaf.R
+++ tests/testthat/test_vaf.R
@@ -1,6 +1,25 @@
 context("Variant allele frequency")
 
 test_that("VAF for rearrangements", {
-  
+  ##
+  ## Load previously identified rearrangements
+  ##
+  skip("Requires ovarian.cell.line.data package")
+  library(svfilters.hg19)
+  library(svbams)
+  library(ovarian.cell.line.data)
+  data(rearrangements_unordered)
+  r <- rearrangements_unordered[["CGOV44T"]]
+  lb <- linkedBins(r)
+  r.chr8 <- r[ chromosome(lb) == "chr8" ]
 
+  ##
+  ## Annotate rearrangements
+  ##
+
+
+
+
+  nsplit <- elementNROWS(splitReads(r.chr8))
+  nimproper <- elementNROWS(lapply(r.chr8, improper))
 })
